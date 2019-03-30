2a9a91708a6cf0fbb5f9a76bb60e3c6f1e2e31ab
Henrique Couto <henrique.coutu@live.com>
2019-03-30 01:22:37 -0300

    navbar pronta

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import logo from '../../assets/logo.svg'
+import './styles.css';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='container cp'>
+          <div className='container'>
+            <div>
+              <a href='/'><img src={logo} alt='logo' /></a>
+            </div>
+            <div className='item'>
+              <a href='/'>Meus anúncios</a>
+            </div>
+            <div className='item'>
+              <a href='/'>Lojas</a>
+            </div><div className='item'>
+              <a href='/'>Ajuda</a>
+            </div>
+          </div>
+          <div className='container'>
+            <div>
+              <a href='/'>
+                <i class="far fa-comment fa-lg" />
+                {' '}
+                Chat
+              </a>
+            </div>
+            <div className='item'>
+              <a href='/'>
+                <i class="far fa-user fa-lg" />
+                {' '}
+                Minha conta
+              </a>
+            </div>
+            <div className='item'>
+              <button>
+                Anunciar
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
