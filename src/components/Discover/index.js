56f337bc3535fa7cf75cf9d581f121d89974ce66
Henrique Couto <henrique.coutu@live.com>
2019-03-31 00:10:36 -0300

    Grid Discover finalizada

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -7,21 +7,33 @@ export default class RecentAds extends Component {
     return (
       <div className='cplr cptb'>
         <h2>Descubra</h2>
-        <div className='container wrap item-disc' style={{ justifyContent: 'flex-start', alignContent: 'flex-start' }} >
-          <div className='container cdc'>
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 390 }} />
+        <div className='container wrap' style={{ justifyContent: 'flex-start', alignContent: 'flex-start' }} >
+          <div className='container cdc item-disc' style={{ width: 270, height: 390 }}>
+            <img src={iphone} className='img-disc' />
           </div>
           <div className='container cdc'>
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 210, marginLeft: 3 }} />
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 177, marginLeft: 3, marginTop: 3 }} />
+            <div className='container cdc item-disc' style={{ width: 260, height: 210, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
+            <div className='container cdc item-disc' style={{ width: 260, height: 175, marginTop: 5, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
           </div>
           <div className='container cdc'>
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 177, marginLeft: 3 }} />
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 210, marginLeft: 3, marginTop: 3 }} />
+            <div className='container cdc item-disc' style={{ width: 260, height: 175, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
+            <div className='container cdc item-disc' style={{ width: 260, height: 210, marginTop: 5, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
           </div>
           <div className='container cdc'>
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 210, marginLeft: 3 }} />
-            <img src={iphone} className='img-disc' style={{ width: 270, height: 177, marginLeft: 3, marginTop: 3 }} />
+            <div className='container cdc item-disc' style={{ width: 260, height: 210, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
+            <div className='container cdc item-disc' style={{ width: 260, height: 175, marginTop: 5, marginLeft: 5 }}>
+              <img src={iphone} className='img-disc' />
+            </div>
           </div>
         </div>
       </div >
